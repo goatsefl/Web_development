3a47554151b189debb4df6714b2c2884520c1eed
goatsefl <swaroopd502@gmail.com>
2024-07-26 00:41:49 +0530

    added new directory, Images in Candy directory.

---
+++ JavaScript/Numbers.js
@@ -0,0 +1,14 @@
+// Primitives in a language are called anything which can store data of different types
+// JS has number,string,boolean,NUll, Undefined and there are technical one's called Symbol and BigInt.
+// Starting with numbers
+// Numbers are either positive and negative (or) whole and decimal.
+
+// 2 , -1, 9.1
+
+// Math Operations (* , + , - , / , % , **) :
+// Multiply : 2 * 3 : 6, Exponentiation : 2 ** 3: 8 (2 raised to the power of 3).
+// Addition : 2 + 3 : 5, Subtraction : 2 - 3 : -1, Modulo : 3 % 2 = 1 (Remainder of the division).
+
+// These basics can be put to use in the chrome browser by inspecting and going through console and dry running with the primitives and math operations.
+// NaN : Not a number, in general this is a number but, it's also not a number in terms of arithmetic operation.
+// For eg : 0/0 is not a number, Nan * 3 is also Nan. In that context, we can understand NaN.

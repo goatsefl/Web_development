a835e7200ac9797c006eb3466777963c9765a6aa
goatsefl <swaroop.shruti123@mgmail.com>
2024-08-10 02:15:12 +0530

    Made changes in Arrays.js by adding more usage material, created Objects.js and added the script inside interactions.html

--- JavaScript/Arrays.js
+++ JavaScript/Arrays.js
@@ -101,3 +101,47 @@ const originalNumbers = [10, 20, 30, 40, 50];
 const removedNumbers = originalNumbers.splice(2, 2, 60, 70); // Removes 2 elements from index 2 and inserts 60, 70
 console.log(originalNumbers); // Output: [10, 20, 60, 70, 50]
 console.log(removedNumbers); // Output: [30, 40]
+
+// JavaScript has a very unique sort method which takes in a basic comparison function to sort it according to the preference of user.
+
+const tropicals = ["banana", "apple", "cherry"];
+fruits.sort();
+console.log(fruits); // Output: ['apple', 'banana', 'cherry']
+
+// In this example, the fruits are sorted in ascending alphabetical order because sort() by default compares elements as strings.
+
+// Same way for numbers but, there's a trick to it.
+
+let numBers = [1, 91, 15, 111, 233, 54421, 1122];
+console.log(numBers.sort());
+
+// Output : (7) [1, 111, 1122, 15, 233, 54421, 91]
+
+// It's because it sorts lexicographically by default. It will sort single digit numbers instantly.
+
+// To sort we use comparison functions with any 2 arguments like shown below.
+
+console.log(numBers.sort((a, b) => a - b));
+
+// Output :[1, 15, 91, 111, 233, 1122, 54421] Ascending order.
+
+console.log(numBers.sort((a, b) => b - a));
+
+// Output :[54421, 1122, 111, 91, 233,15 ,1] Descending order.
+
+// We can use sort function to customize our sorting style, that makes the javascript sort powerful and useful for different scenarios.
+
+// Multi-Dimensional Arrays Eg:
+
+// Creating Multi-Dimensional array :
+let matrix = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
+];
+
+let element = matrix[1][2]; // Accesses the element at row 1, column 2 (which is 6)
+
+matrix.push(10, 11, 12);
+
+// Adding new inner array.
---
+++ JavaScript/Objects.js
@@ -0,0 +1,40 @@
+// Objects : Unordered collection of key-value pairs.
+
+// Creating an object :
+let objectName = { key1: "value1", key2: "value2" };
+
+// Dot notation
+console.log(objectName.key1);
+// Bracket notation
+console.log(objectName["key2"]);
+// Output : value1
+
+// All keys are intrinsically converted to strings.
+
+// We can create new keys by using dot or bracket notation
+
+objectName.key3 = "value3";
+objectName["key4"] = "value4";
+
+// Output : {key1: 'value1', key2: 'value2', key3: 'value3', key4: 'value4'}
+
+// Nested Array Objects
+
+const school = {
+	name: "Wonderland High",
+	classes: [
+		{
+			subject: "Math",
+			students: ["Alice", "Bob", "Charlie"],
+		},
+		{
+			subject: "Science",
+			students: ["David", "Eva", "Frank"],
+		},
+	],
+};
+
+console.log(classroom[0][1]); // Outputs: Bob (second student in the first group)
+console.log(classroom[2][0]); // Outputs: Grace (first student in the third group)
+
+// We use index notation and dot notation to get access to different values in objects in arrays and vice versa.

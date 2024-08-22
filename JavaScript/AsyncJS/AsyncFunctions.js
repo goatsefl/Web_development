e34f945c9811d160b1b418074a786ec91a2e02e6
goatsefl <swaroopd502@gmail.com>
2024-08-22 23:59:06 +0530

    Made Changes in AsyncFunctions and async.html

--- JavaScript/AsyncJS/AsyncFunctions.js
+++ JavaScript/AsyncJS/AsyncFunctions.js
@@ -92,3 +92,62 @@ login("kjk", "")
 	.catch((err) => {
 		console.log("Type in correct credentials : ", err);
 	});
+
+// await() : We can only use this keyword inside async declared functions.
+// await will pause the execution of the function waiting for promise to be resolved.
+
+// Let's use an example of await to understand.
+
+const randomColors = () => {
+	const first = Math.floor(Math.random() * 256);
+	const second = Math.floor(Math.random() * 256);
+	const third = Math.floor(Math.random() * 256);
+	const alpha = Math.random();
+	return `rgba(${first},${second},${third},${alpha})`;
+};
+
+const randomTransitions = () => {
+	const x1 = Math.random();
+	const y1 = Math.random();
+	const x2 = Math.random();
+	const y2 = Math.random();
+	return `1.5s background-color cubic-bezier(${x1},${y1},${x2},${y2})`;
+};
+
+const changer = (delay) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if (delay < 800) {
+				document.body.style.backgroundColor = randomColors();
+				document.body.style.transition = randomTransitions();
+				resolve(`Random color added`);
+			} else {
+				reject(`Connection Timed Out`);
+			}
+		}, delay);
+	});
+};
+let bool = false;
+const randomColorChanger = async (bool) => {
+	while (bool) {
+		let data = await changer(Math.floor(Math.random() * 1500));
+	}
+	console.log(data1);
+};
+randomColorChanger();
+
+// If our await is throwing an error, we can store it in a variable and use the try catch block to catch some error that is stored in the data.
+
+async function great() {
+	try {
+		await console.log("gagagaga");
+		afl;
+		akjsdfkljaksdjfk;
+		lj;
+	} catch (e) {
+		console.error("Relax boss, it's completely fine, it's a ", e);
+	}
+}
+
+// Output : gagagaga
+// Relax boss, it's completely fine, it's a Reference error afl is not defined at great.

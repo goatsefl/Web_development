c8f2bd61acedccef87262f005b85ed48e541c2ee
goatsefl <swaroopd502@gmail.com>
2024-08-23 23:15:23 +0530

    Addedd 3 new files and made some changes and added required explanation for API's

---
+++ JavaScript/AJAX/intro.js
@@ -0,0 +1,21 @@
+// Ajax is Asynchronous Javascript and XML.
+// Making requests or get request without interrupting the flow of the page i.e. reloading the page.
+
+// Every Modern webpage has an AJAJ where J stands for JSON.
+
+// Those infinite scrolls are the wonder of Asynchronous code, and to see the requests we can use chrome developer tools.
+// Navigate to network, that will show the network traffic of requests made to the server of the main domain.
+
+// Live search is also a request you're sending it to server to quickly fetch the relevant search result.
+
+// Using an API we can send the relevant data, that is packaged in a JSON file.
+// JSON is called JavaScript Object Notation is a file that is used to transfer relevant data that is human and machine-readable.
+
+// We need basic data that is stored in JSON file, how do we access it?
+// We use API's (Application User Interface) to communicate to different PC's to send/request data.
+// We use WEB API to send/request data from different API providers in internet. These API's use HTTP, different companies like google, reddit and facebook have API's for developer's use case.
+// In simple words, it's all data that we need, and API is important to get/provide data.
+
+// These web API's are some URL's that are called the endpoint to send/request data according to the docs.
+
+// openweathermap.org/api is an api providing weather info, if there's a requirement of extensive weather data, then there'll be different prices.

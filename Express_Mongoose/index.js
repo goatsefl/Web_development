9845015fddd0e45956eccd0ddc9bfbe23a179e13
goatsefl <swaroopd502@gmail.com>
2024-11-28 23:06:57 +0530

    Made new directory to make an entire webPage

--- Express_Mongoose/index.js
+++ Express_Mongoose/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const mongoose = require('mongoose')
+const Product = require('./models/product')
+mongoose.connect('mongodb://localhost:27017/newApp', { useNewUrlParser: true }).
+    then(() => {
+        console.log("Connection is Working on Mongo")
+    }).
+    catch(e => {
+        console.log("OOPS Connection issue")
+        console.log(e)
+    })
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
+app.listen(3000, () => {
+    console.log("Listening on PORT 3000!!")
+})
---
+++ Express_Mongoose/seeds/seeds.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Product = require('../models/product')
+mongoose.connect('mongodb://localhost:27017/newApp').
+    then(() => {
+        console.log("Connection is Working on Mongo")
+    }).
+    catch(e => {
+        console.log("OOPS Connection issue")
+        console.log(e)
+    })
+
+const p = new Product({
+    name: "Grape Fruit",
+    price: 1.99,
+    category: 'fruit'
+})
+// p.save().then(p => console.log(p))
+
+const seedProducts = [{}, {}, {}, {}, {}, {}]
